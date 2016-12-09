7155c73e36748abebcfc718181d9b91610294690
DESKTOP-L4FMVUO\DacoTacoFlame <davidwcorso@gmail.com>
2016-12-09 17:12:15 -0500

    BT25 TankMovementComponent Setup

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -7,17 +7,6 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	auto ControlledTank = GetControlledTank();
-
-	if (!ControlledTank) 
-	{
-		// UE_LOG(LogTemp, Warning, TEXT("No tank :("));
-	}
-	else 
-	{
-		// UE_LOG(LogTemp, Warning, TEXT("TANK!!!!!!!! %s"), *(ControlledTank->GetName()));
-	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
